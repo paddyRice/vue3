c9a214d495f9d7ab7d9a32abaa99e646a1491d5a
paddyRice <webliujs@gmail.com>
2025-01-13 20:26:38 +0800

    1

---
+++ vue-base/src/views/GoBang.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="box"></div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+    .box{
+        width: 100px;
+        height: 100px;
+        background: pink;
+        box-shadow: 0 0 10px;
+    }
+</style>
